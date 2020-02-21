2a5fd58c04c0f21a61c4ed16a7de14aa4a16c277
Smoosh1 <macievskyivan@gmail.com>
2020-02-22 01:11:33 +0200

    vec

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector vec1 = new Vector(1, 1, 1);
+            Vector vec2 = new Vector(1, 1, 1);
+
+            Vector sum = new Vector { VecSum = $"{vec1 + vec2}" };
+            Console.WriteLine(sum);
+        }
+        private string VecSum;
+        public override string ToString() => VecSum;
+}
+    }
---
+++ VectorStruct.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+
+    public struct Vector
+    {
+        float x, y, z;
+        
+
+        public Vector(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public static Vector operator +(Vector a, Vector b)
+        {
+            Vector result;
+            result.x = a.x + b.x;
+            result.y = a.y + b.y;
+            result.z = a.z + b.z;
+            return result;
+        }
+        /*public static Vector operator -(Vector a, Vector b)
+        {
+
+        }
+        public static Vector operator *(Vector a, Vector b)
+        {
+ 
+        }
+        public static Vector operator /(Vector a, Vector b)
+        {
+
+        }*/
+        public static Vector VectorUp(Vector a)
+        {
+            a.y++;
+            return a;
+        }
+
+        
+    }
+}
